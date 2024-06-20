093a4b19c0a1782c0c63536ed534e60b2b24976b
Anonscodex <normalstuff2001@gmail.com>
2024-06-20 07:43:46 -0700

    learn aliases and interface and also union types

--- hello.js
+++ hello.js
@@ -5,6 +5,11 @@ console.log(message + " in " + year + "i am " + clas);
 var u = true;
 u = "string";
 console.log(u);
+var names = [];
+names.push("Anons");
+console.log(names);
+var films = [];
+//this is readonly you cant add to it
 var myCollection;
 myCollection = [5, "hello", 4, true];
 console.log(myCollection);
@@ -26,3 +31,37 @@ var Anime;
 })(Anime || (Anime = {}));
 var favAnime = Anime.HunterXHunter;
 console.log("The value of this anime is " + favAnime);
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+    StatusCodes[StatusCodes["Success"] = 200] = "Success";
+    StatusCodes[StatusCodes["Accepted"] = 202] = "Accepted";
+    StatusCodes[StatusCodes["BadRequest"] = 400] = "BadRequest";
+})(StatusCodes || (StatusCodes = {}));
+console.log(StatusCodes.NotFound);
+console.log(StatusCodes.Accepted);
+var CarYear = 2001;
+var CarType = "Toyota";
+var CarModel = "Corolla";
+var car = {
+    year: CarYear,
+    type: CarType,
+    model: CarModel
+};
+console.log(car);
+var rectangle = {
+    height: 30,
+    width: 20
+};
+console.log(rectangle);
+var coloredPerson = {
+    country: "Nigeria",
+    age: 20,
+    color: "black"
+};
+console.log(coloredPerson);
+function printMyResult(result) {
+    console.log("Your result is ".concat(result));
+}
+printMyResult(90);
+printMyResult("A");
--- hello.ts
+++ hello.ts
@@ -8,6 +8,13 @@ let u : any = true;
 u = "string";
 console.log(u) 
 
+const names : string[] = []
+names.push("Anons")
+console.log(names)
+
+const films: readonly string[] = []
+//this is readonly you cant add to it
+
 let myCollection : [number, string, number, boolean];
 myCollection = [5,"hello",4, true]
 console.log(myCollection)
@@ -31,3 +38,71 @@ enum Anime {
 }
 let favAnime = Anime.HunterXHunter
 console.log ("The value of this anime is " + favAnime )
+
+enum StatusCodes {
+    NotFound = 404,
+  Success = 200,
+  Accepted = 202,
+  BadRequest = 400
+}
+
+console.log(StatusCodes.NotFound)
+console.log(StatusCodes.Accepted)
+
+type CarYear = number
+type CarType = string
+type CarModel = string 
+
+type Car = {
+    year : CarYear
+    type : CarType
+    model : CarModel
+}
+
+const CarYear: CarYear = 2001
+const CarType: CarType ="Toyota"
+const CarModel: CarModel = "Corolla"
+
+const car : Car = {
+    year: CarYear,
+    type : CarType,
+    model: CarModel
+}
+
+console.log(car)
+
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+const rectangle : Rectangle = {
+    height: 30,
+    width: 20
+}
+
+console.log(rectangle)
+
+interface Person {
+    country: string,
+    age: number
+}
+
+interface ColoredPerson extends Person {
+    color: string
+}
+
+const coloredPerson : ColoredPerson = {
+    country: "Nigeria",
+    age: 20,
+    color: "black"
+}
+
+console.log(coloredPerson)
+
+function printMyResult (result : string|number) {
+    console.log(`Your result is ${result}`)
+}
+
+printMyResult(90)
+printMyResult("A")
